5333fbbe423aed93bc5b9e93ec94375e7a0a58dc
Cristime <thecristime@outlook.com>
2020-10-07 20:50:10 +0800

    First C0mmit

---
+++ Chapter1/1/1.cpp
@@ -0,0 +1,11 @@
+// 1.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Name: Cristime" << endl;
+	cout << "Address: thecristime@outlook.com" << endl;
+
+	return 0;
+}
---
+++ Chapter1/2/2.cpp
@@ -0,0 +1,14 @@
+// 2.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double distance;
+	cout << "Enter the distance: ";
+	cin >> distance;
+	distance /= 220;
+	cout << distance << endl;
+	
+	return 0;
+}
